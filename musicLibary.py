9983adabc811fe957981551ccc727ab39dbb9baa
0123-hamn <hemantaghosh009@gmail.com>
2025-04-12 12:44:46 +0530

    Jarvis

---
+++ musicLibary.py
@@ -0,0 +1,6 @@
+music = {
+    "jhol" : "https://www.youtube.com/watch?v=BWczaSneA0Q",
+    "pokemon" : "https://www.youtube.com/watch?v=fCkeLBGSINs",
+    "let it go" : "https://www.youtube.com/watch?v=L0MK7qz13bU",
+    " hanuman chalisa" : "https://www.youtube.com/watch?v=AETFvQonfV8"
+}
